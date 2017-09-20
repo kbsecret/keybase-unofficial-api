ecba30c6896cd7c17f38302c8825ea8e7f5160da
William Woodruff <william@tuffbizz.com>
2017-09-19 22:37:42 -0400

    api: Fill in documentation

--- lib/keybase/api.rb
+++ lib/keybase/api.rb
@@ -4,8 +4,10 @@ require "keybase/core"
 
 require_relative "api/api"
 
+# The primary namespace.
 module Keybase
   module API
+    # The current version of `keybase-unofficial-api`.
     VERSION = "1.0.0"
   end
 end
